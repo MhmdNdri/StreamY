badd78a4b5fe0d27afa54d5b9824ab915d16e8c0
mhdmndri <m.n.mohammad.naderi@gmail.com>
2021-01-06 03:17:33 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './styles/App.css';
+import { BrowserRouter, Route } from "react-router-dom"
+import Header from './components/Header';
+import StreamCreate from './components/streams/StreamCreate'
+import StreamDelete from './components/streams/StreamDelete'
+import StreamEdit from './components/streams/StreamEdit'
+import StreamList from './components/streams/StreamList'
+import StreamShow from './components/streams/StreamShow'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/update" exact component={StreamEdit} />
+          <Route path="/streams/show" exact component={StreamShow} />
+          <Route path="/streams/login" exact component={StreamCreate} />
+        </div>
+      </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
